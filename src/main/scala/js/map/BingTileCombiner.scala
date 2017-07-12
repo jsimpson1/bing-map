528ed737172c978f78f0ea2450a3adfb9dd01575
Jeremy Simpson <jeremy@accur8software.com>
2017-07-11 20:56:06 -0400

    clean up

--- src/main/scala/js/map/BingTileCombiner.scala
+++ src/main/scala/js/map/BingTileCombiner.scala
@@ -10,17 +10,27 @@ object BingTileCombiner extends App {
 
   apply(WSG84Coordinate(39.812515, -77.276845), WSG84Coordinate(39.775253, -77.211525), 17)
 
-  def apply(startPoint: WSG84Coordinate, endPoint: WSG84Coordinate, levelOfDetail: Int) = {
+  def apply(startPoint: WSG84Coordinate, endPoint: WSG84Coordinate, levelOfDetail: Int): Unit = {
 
-    val tiles = BingMapUtility.getTilesAndHtmlValues(startPoint, endPoint, levelOfDetail)
+    val tiles =
+      BingMapUtility
+        .getTilesAndHtmlValues(startPoint, endPoint, levelOfDetail)
 
+    val width =
+      tiles
+        .map( t => t._2.x + t._2.size )
+        .max
 
-    val width = tiles.map( t => t._2.x + t._2.size ).max
-    val height = tiles.map( t => t._2.y + t._2.size ).max
+    val height =
+      tiles
+        .map( t => t._2.y + t._2.size )
+        .max
 
     val compositeImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
 
-    val gr = compositeImage.createGraphics()
+    val gr =
+      compositeImage
+        .createGraphics()
 
     tiles.foreach { case (qk, tileInfo) =>
       val tileImage = ImageIO.read(new java.io.File(s"./tiles/${qk.value}.jpeg"))
--- src/main/scala/js/map/CreateMapFromBingTiles.scala
+++ src/main/scala/js/map/CreateMapFromBingTiles.scala
@@ -14,7 +14,7 @@ object CreateMapFromBingTiles extends App {
   apply(WSG84Coordinate(39.812515 + sizeOfOriginalMap , -77.276845), WSG84Coordinate(39.775253, -77.211525), 17)
   //  apply(WSG84Coordinate(39.812515, -77.276845), WSG84Coordinate(39.775253, -77.211525), 17) // map until 7/1/2017
 
-  def apply(startPoint: WSG84Coordinate, endPoint: WSG84Coordinate, levelOfDetail: Int) = {
+  def apply(startPoint: WSG84Coordinate, endPoint: WSG84Coordinate, levelOfDetail: Int): Unit = {
     BingTileDownloader.apply(startPoint, endPoint, levelOfDetail)
     BingTileCombiner.apply(startPoint, endPoint, levelOfDetail)
     println("map is ./composite.jpeg")
